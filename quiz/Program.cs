053aff39f2eb9703bb646875607966055d256564
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2024-09-10 12:56:09 +0200

    skibidi

--- quiz/Program.cs
+++ quiz/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//string name = "Alpha";
+
+
+//bool isAlive = name == "Alpha";
+
+
+//int hp = 100;
+
+//hp += 10;
+
+//hp++;
+
+
+
+//Console.WriteLine($"Du har {hp} hp kvar");
+
+//if (hp > 0)
+//{
+    //Console.WriteLine("du är sigma!");
+
+//}
+
+//jag ville bara ha kvar min kod för jag tyckte om den :)
+
+
+
+
+
+
+
+Console.ReadLine();
