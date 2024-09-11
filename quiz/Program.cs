83d43d0d15f75c6d0a13fa47ed075dec60a1bcf7
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2024-09-11 14:41:34 +0200

    skibidid skibidi sigma yippie

--- quiz/Program.cs
+++ quiz/Program.cs
@@ -34,25 +34,57 @@ string sigmasvaret = Console.ReadLine();
 if (sigmasvaret == "a")
 {
     rätt++;
-    Console.WriteLine("DU hade RÄTT!! DU är verkerligen en potenciell sigma:3");
+    Console.WriteLine("rätt");
 }
 else
 {
-    Console.WriteLine("Attans, Du hade fel :( Verkar som att du inte är sigman som jag trodde du var.)");
+    Console.WriteLine("fel");
 }
 Console.WriteLine("Fråga 2, ifall du möter en annan sigma på stan vad gör du?");
 Console.WriteLine("a, du börjar ryta om grymta för att visa att du är alphan och tar kontroll över ditt revir! b, Du börjar looksmaxa och moggar den andra sigman för att visa att du är mer sigma än vad den andra är! c, Du gräpper tag i den andra sigmans underliv och känner ifall du kan vinna denna utmaning eller inte!");
 string skibidisvaret = Console.ReadLine();
-if (sigmasvaret == "c")
+if (skibidisvaret == "c")
 {
     rätt++;
-    Console.WriteLine("Du är nu 50% mer sigma än vad den generalla populationen verkligen simga av dig");
+    Console.WriteLine("rätt");
 
 }
 else
 {
-    Console.WriteLine("cDet är dags för dig att lämna innan jag släpper ut min sigma på dig, Vänta du får ett försök till jag tror på dig sigma");
+    Console.WriteLine("fel");
 }
+
+Console.WriteLine("""Nu är det dags för den sista frågan! Det är dags för oss att bli lite freaky >:3  Ifall du någonsin möter dig i en intim situation med en partner vad gör du? a, Du hittar närmaste spegel och börjar titta på dig själv medans du låtsas att du har sex med dig själv! b, Du fokuserar på din partner och ser till så att dom har det så bra dom kan! c, En riktig sigma skulle aldrig ha en intim sittuation eftersom att han hade moggat den andra så hårt så dom inte skulle våga gå nära!""");
+string sannasigman = Console.ReadLine();
+if (sannasigman == "a")
+{
+    rätt++;
+    Console.WriteLine("rätt");
+    
+}
+else
+{
+    Console.WriteLine("fel");
+}
+if (rätt > 2)
+{
+    Console.WriteLine("WOWZERS, DU ÄR VERKERLIGEN SIGMAN JAG TRODDE DU VAR X3, NÅGON KAN MOGGA DOM ANDRA KAN JAG SE, DU HAR NU FÖRTJÄNAT DIN SIGMA BADGE I FORM AV konata från den populära serien lucky starr!!");
+    skibidi();
+
+
+}
+if (rätt > 1)
+{
+    Console.WriteLine("Du är väl okej, inte lika sigma som jag hoppades men du är väl åtminstånde inte som vissa, du borde acceptera att ditt liv aldrig kommer bli bättre än vad det är just nu, ifall kan acceptera det så borde du ta bort dig själv från våran jord innan du ser dig slälv falla från platsen du står på:(");
+
+}
+if (rätt == 0)
+{
+   krillion();
+}
+
+
+
 
 Console.WriteLine($"Du har {rätt} rätt");
 
@@ -63,4 +95,61 @@ Console.WriteLine($"Du har {rätt} rätt");
 
 
 
+
+
 Console.ReadLine();
+
+
+
+
+
+
+static void krillion() 
+{
+    Console.WriteLine(""" 
+                                    ,--,      ,--,                                                                                       ,--,              
+       ,--.                  ,---.'|   ,---.'|                            ,----..                                                 ,---.'|              
+   ,--/  /|,-.----.     ,---,|   | :   |   | :                           /   /   \                ,-.----.    .--.--.       ,---,.|   | :       ,---,. 
+,---,': / '\    /  \ ,`--.' |:   : |   :   : |                   ,---,  /   .     :          ,--, \    /  \  /  /    '.   ,'  .' |:   : |     ,'  .' | 
+:   : '/ / ;   :    \|   :  :|   ' :   |   ' :                  /_ ./| .   /   ;.  \       ,'_ /| ;   :    \|  :  /`. / ,---.'   ||   ' :   ,---.'   | 
+|   '   ,  |   | .\ ::   |  ';   ; '   ;   ; '            ,---, |  ' :.   ;   /  ` ;  .--. |  | : |   | .\ :;  |  |--`  |   |   .';   ; '   |   |   .' 
+'   |  /   .   : |: ||   :  |'   | |__ '   | |__         /___/ \.  : |;   |  ; \ ; |,'_ /| :  . | .   : |: ||  :  ;_    :   :  |-,'   | |__ :   :  :   
+|   ;  ;   |   |  \ :'   '  ;|   | :.'||   | :.'|         .  \  \ ,' '|   :  | ; | '|  ' | |  . . |   |  \ : \  \    `. :   |  ;/||   | :.'|:   |  |-, 
+:   '   \  |   : .  /|   |  |'   :    ;'   :    ;          \  ;  `  ,'.   |  ' ' ' :|  | ' |  | | |   : .  /  `----.   \|   :   .''   :    ;|   :  ;/| 
+|   |    ' ;   | |  \'   :  ;|   |  ./ |   |  ./            \  \    ' '   ;  \; /  |:  | | :  ' ; ;   | |  \  __ \  \  ||   |  |-,|   |  ./ |   |   .' 
+'   : |.  \|   | ;\  \   |  ';   : ;   ;   : ;               '  \   |  \   \  ',  / |  ; ' |  | ' |   | ;\  \/  /`--'  /'   :  ;/|;   : ;   '   :  '   
+|   | '_\.':   ' | \.'   :  ||   ,/    |   ,/                 \  ;  ;   ;   :    /  :  | : ;  ; | :   ' | \.'--'.     / |   |    \|   ,/    |   |  |   
+'   : |    :   : :-' ;   |.' '---'     '---'                   :  \  \   \   \ .'   '  :  `--'   \:   : :-'   `--'---'  |   :   .''---'     |   :  \   
+;   |,'    |   |.'   '---'                                      \  ' ;    `---`     :  ,      .-./|   |.'               |   | ,'            |   | ,'   
+'---'      `---'                                                 `--`                `--`----'    `---'                 `----'              `----'     
+                                                                                                                                                       
+""");
+}
+
+
+ 
+static void skibidi()
+{
+Console.WriteLine(""" 
+　　　　　　 　 　 　 _.　-　._　　　　　　　　　　　　 _
+　　　　　　　　　　　　　　　　　　　　　　／.　-－- ＼　　　　　　 　 　 　 l ｀i　　　＿_
+　　　　　　　　　　　　 　 　 　 　 　 　 /／　　　　　　ヽ　　　　　　　　　　}　´￣　´ '´ う
+　　　　　　　　　　　　　　　　　　　　　|｛　　　　　 ／＼　　＿＿＿＿　　j　　　　　／
+　　　　　 （ ヽ　　　　　　　　　　　　　 '.l.　　　___/　 　 ヽ´　　ー-=ﾆ.¨｀７ 　 　 ｒ '
+　　 r──’　｀ヽ　　　　　　　　　　　　.ゞ ' ´　　 　 　 　 '.　　　　　　 ｀丶、　 /
+　(´_￣　　　　 　 ヽ　　　　　　　　 ／　　　　　　 　 　 　 |　　　　 　 ＼　 ＼/
+　‘ｰ⊂.　　 　 　 　 ＼　　　　　　/　 ,　　　 ｌ　 ｜　　　　|、　　　 　 　 ヽ‐-,ヽ
+　　　　　￣￣＼ 　 　 ＼. 　 　 /　 /　　 　 !　 ∧ 　 ｜｜ヽ__｜　　　　 ∨　｀
+　　　　　　　　　 ＼ 　 　 ＼　 /　 /　　　　 |　/　' 　 ｜｜'´ヽ l　 　 　 l.　',
+　　　　　　　　　 　 ＼ 　 　 ＼l　 '　 　 |　 ,ィ´′ ∨　ﾊ. |　　 Nヽ.　　　|､　i
+　　　　　　　　　 　 　 ＼ 　 　 ＼l.　 　 |　 /|/　 　 　/　/ 　 　 　 ',　　 |､ヽ!
+　　　　　　　　　 　 　 　 ＼ 　 　 ヽ､.　 |　i 　 　 　 ∨　　　 三三 ハ.　 !　＼
+　　　　　　　　　 　 　 　 　 ＼.　　 /＼ |　|　ｘィ彡　　　　　　　　・{　l.　∧　/
+　　　　　　　　　　　　　　　　/＼ /　　 }'ヽ! "´　　　　 　 ,、_,　　　l　|∨　∨
+　　　　　　　　　 　 　 　 　 /　／　　　/ 　 ＼　　 　 ‘７´　 ） 　 .ﾉ　| 　 　 l
+　　　　　　　　　　　　　　　{　　　　　 /　　　 ',＼　　　 ､__,. '　,／　　|　 　 /
+　　　　　　　　　　　　 　 　 ＼　　 ／　　　 　 ',　ヽ----r　' ´　|　　 |　　./
+　　　　　　　　　　　　　　　　 | ｰ ´　　 　 　 　 ',　 ',　　 ヽ　 　 |　　 |　 /
+""");
+}
+
